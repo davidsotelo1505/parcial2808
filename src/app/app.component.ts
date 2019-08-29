b00b9559a2589942263587f9b97fce5f5308933d
Wendy T <salas@unilibre.edu.co>
2019-08-28 21:36:55 -0500

    parcial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Parcial2808';
+  elementos:any = [
+    {nombre: 'Azufre', simbolo:'S', atomico: '16', peso: '32,065', transicion:2 },
+    {nombre: 'Hidrogeno', simbolo:'H', atomico: '1', peso: '15,65', transicion:1},
+    {nombre: 'Oro', simbolo:'O', atomico: '79', peso: '10,9', transicion:2},
+    {nombre: 'Plata', simbolo:'P', atomico: '41', peso: '13', transicion:1},
+    {nombre: 'Maganesio', simbolo:'M', atomico: '74', peso: '12', transicion:1},
+    {nombre: 'Mercurio', simbolo:'Mer', atomico: '10', peso: '85', transicion:1},
+    {nombre: 'Plomo', simbolo:'Plo', atomico: '45', peso: '23', transicion:2},
+    {nombre: 'Calcio', simbolo:'Cal', atomico: '78', peso: '5', transicion:2},
+    {nombre: 'Carbono', simbolo:'C', atomico: '12', peso: '7,4', transicion:2}
+  ]
 }
+
+
